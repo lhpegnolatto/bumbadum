bceb71c310dd814438357b9550762bf1a4e3fd00
Luiz Henrique Pegnolatto <lhpegnolatto@gmail.com>
2023-07-06 09:07:02 -0300

    feat: adding mute button on player

--- apps/web/src/components/Player.tsx
+++ apps/web/src/components/Player.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import YouTubePlayer from "youtube-player";
 import { YouTubePlayer as YouTubePlayerType } from "youtube-player/dist/types";
 import { useSocket } from "@/hooks/useSocket";
+import { SpeakerSimpleHigh, SpeakerSimpleSlash } from "@/components/Icons";
 
 function formatTime(time: number): string {
   const minutes = Math.floor(time / 60);
@@ -25,6 +26,9 @@ export function Player() {
   const [currentTime, setCurrentTime] = useState(0);
   const [music, setMusic] = useState<any>(null);
 
+  const [isMuted, setIsMuted] = useState(false);
+  const [volume, setVolume] = useState<number | undefined>(undefined);
+
   const percent = Math.round((currentTime / duration) * 100);
 
   useEffect(() => {
@@ -33,6 +37,19 @@ export function Player() {
       width: 0,
       height: 0,
     });
+
+    (async () => {
+      const playerVolume = await playerRef.current?.getVolume();
+
+      setVolume((old) => {
+        if (!old) {
+          return playerVolume;
+        }
+
+        return old;
+      });
+    })();
+
     playerRef.current.playVideo();
 
     playerRef.current.on("stateChange", (e) => {
@@ -71,6 +88,12 @@ export function Player() {
     }, 1000);
   }, []);
 
+  useEffect(() => {
+    if (playerRef.current && typeof volume === "number") {
+      playerRef.current.setVolume(isMuted ? 0 : volume);
+    }
+  }, [volume, isMuted]);
+
   return (
     <div>
       <Head>
@@ -93,22 +116,35 @@ export function Player() {
             </div>
 
             <div className="flex items-center gap-3 p-6">
-              <div className="h-10 w-10 overflow-hidden rounded-lg">
+              <div className="flex h-10 w-10 items-center justify-center overflow-hidden rounded-lg bg-black">
                 <Image
                   src={music?.thumb}
                   width={40}
                   height={40}
                   alt="Nevermind album cover"
+                  style={{ objectFit: "fill" }}
                 />
               </div>
-              <div className="flex flex-1 flex-col justify-center">
-                <p className="text-sm font-semibold text-white">
+              <div className="flex flex-1 flex-col justify-center gap-1">
+                <p
+                  className="max-w-[140px] overflow-hidden text-ellipsis whitespace-nowrap text-sm font-semibold text-white"
+                  aria-label={music?.title}
+                >
                   {music?.title}
                 </p>
                 <p className="text-xs text-gray-400">{music?.channel}</p>
               </div>
-              <div className="text-xs text-gray-400">
-                {formatTime(currentTime)}/{formatTime(duration)}
+              <div className="flex flex-col items-end justify-center">
+                <div className="text-xs text-gray-400">
+                  {formatTime(currentTime)}/{formatTime(duration)}
+                </div>
+
+                <button
+                  className="p-1 text-white"
+                  onClick={() => setIsMuted((old) => !old)}
+                >
+                  {isMuted ? <SpeakerSimpleSlash /> : <SpeakerSimpleHigh />}
+                </button>
               </div>
             </div>
           </>
